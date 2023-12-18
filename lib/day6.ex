5996d6d0a9ab0bb0131457076b41247c0b125b37
Stefan Kupych <stefan@hifyre.com>
2023-12-18 15:49:56 -0500

    Day 6

--- lib/day6.ex
+++ lib/day6.ex
@@ -10,13 +10,23 @@ defmodule Aoc.Day6 do
   def day(), do: 6
 
   @impl Day
-  def a(_) do
-    :not_solved
+  def a(races) do
+    races
+    |> Enum.map(&process_line/1)
+    |> Enum.zip()
+    |> Enum.map(&process_race/1)
+    |> Enum.map(&Enum.count/1)
+    |> Enum.product()
   end
 
   @impl Day
-  def b(_) do
-    :not_solved
+  def b(race) do
+    race
+    |> Enum.map(&process_line_b/1)
+    |> Enum.zip()
+    |> hd()
+    |> process_race()
+    |> Enum.count()
   end
 
   @impl Day
@@ -25,5 +35,29 @@ defmodule Aoc.Day6 do
       file
       |> String.split("\n", trim: true)
     end
+  end
+
+  def process_race({time, distance}) do
+    1..time
+    Enum.filter(1..time, &will_beat_record?(&1, time, distance))
+  end
+
+  def will_beat_record?(speed, record, distance) do
+    (speed * (record - speed)) >= distance
+  end
+
+  defp process_line(line) do
+    line
+    |> String.split(~r/\s+/, trim: true)
+    |> tl()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp process_line_b(line) do
+    line
+    |> String.replace(~r/\s+/, "")
+    |> String.split(":", trim: true)
+    |> tl()
+    |> Enum.map(&String.to_integer/1)
   end
 end
